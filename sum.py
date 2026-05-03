a21283e3b0b6f0399530aba4caa7da5d3ad8168e
akshat-working <akshattalwar100@gmail.com>
2026-05-03 16:40:14 +0530

    normal

--- sum.py
+++ sum.py
@@ -1,10 +1,5 @@
-def add(x, y):
-    if y == 0:
-        return x
-    elif y > 0:
-        return add(x + 1, y - 1)
-    else:
-        return add(x - 1, y + 1)
-a = -2
-b = 7
+def add(x,y):
+  return a+b
+a=-2
+b=7
 print(add(a,b))
